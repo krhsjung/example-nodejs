71e1c53e0ad98b23a7469be0b4fd97009cffde2c
krhsjung <kr.hs.jung@gmail.com>
2024-07-08 16:03:22 +0900

    [MOD] routing strucure for web application

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -1,9 +1,13 @@
 import express from "express";
+import apiRouter from "./routes/routes.js"
 
 const app = express();
 const port = 3000;
 
+// health 체크
 app.get("/", (req, res) => res.sendStatus(200));
+
+app.use("/api", apiRouter)
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
