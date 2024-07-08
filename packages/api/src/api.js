749745de7e9afd72d7f8ac38d7e8cf458ef67146
krhsjung <kr.hs.jung@gmail.com>
2024-07-08 14:33:10 +0900

    [ADD] express for web application

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -1,3 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.sendStatus(200));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
 export default function test() {
   return 'Hello from api';
 }
