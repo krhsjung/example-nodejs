e10cdbe44d5260201718b238ecb40ccb41dfb7e3
krhsjung <kr.hs.jung@gmail.com>
2024-07-09 17:30:59 +0900

    [ADD] body-parser (HTTP request body parsing middleware)

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -9,6 +9,10 @@ const port = 3000;
 // HTTP request logger middleware 추가
 app.use(morgan("dev"));
 
+// request body를 json 형태로 변환하기 위한 body-parser 적용
+app.use(express.json());                          // parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));  // parse application/json
+
 // health 체크
 router.get("/", (req, res) => res.sendStatus(200));
 router.use("/api", apiRouter);
