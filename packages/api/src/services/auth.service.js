7fc5a51a13889d9bacb9b3c0d04378717411d989
krhsjung <kr.hs.jung@gmail.com>
2024-09-20 11:07:43 +0900

    [MOD] exchange token expire value(duration -> timestamp)

--- packages/api/src/services/auth.service.js
+++ packages/api/src/services/auth.service.js
@@ -1,4 +1,4 @@
-import { keycloakApis, ResponseType } from 'lib'
+import { jwt, keycloakApis, ResponseType } from 'lib'
 
 class AuthService {
   async exchangeToken({ token, issuer }) {
@@ -8,10 +8,20 @@ class AuthService {
       console.log(`HTTP error! status: ${response.status}, body: ${JSON.stringify(await response.json())}`);
       return ResponseType.FAIL_EXCHANGE_TOKEN;
     }
+
+    const exchangedToken = await response.json();
+
+    const decodedToken = jwt.decode(exchangedToken.access_token);
+    exchangedToken['expires_in'] = decodedToken.exp
+
+    const decodedRefreshToken = jwt.decode(exchangedToken.refresh_token);
+    exchangedToken['refresh_expires_in'] = decodedRefreshToken.exp
+
+    console.log(exchangedToken);
     
     return {
       ...ResponseType.OK,
-      data: await response.json()
+      data: exchangedToken
     };
   };
 }
